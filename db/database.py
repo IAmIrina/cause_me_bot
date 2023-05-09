979ad31786015a73b50cc399c757d01c643e4c75
Irina <irina.vl.kiseleva@gmail.com>
2023-05-09 12:06:41 +0300

    Change SA_KEY credentials to MetadataUrlCredentials

--- db/database.py
+++ db/database.py
@@ -12,15 +12,11 @@ def get_driver():
     driver = ydb.Driver(
         endpoint=settings.ydb.endpoint,
         database=settings.ydb.database,
-        credentials=ydb.iam.ServiceAccountCredentials.from_file(
-            settings.ydb.sa_key_file,
-        ),
+        credentials=ydb.iam.MetadataUrlCredentials(),
     )
     try:
         driver.wait(fail_fast=True, timeout=5)
     except TimeoutError as err:
-        logger.error("Connect failed to YDB")
-        logger.error("Last reported errors by discovery:")
-        logger.error(driver.discovery_debug_details())
+        logger.error("Connect failed to YDB. Last reported errors by discovery: %s", driver.discovery_debug_details())
         raise err
     return driver

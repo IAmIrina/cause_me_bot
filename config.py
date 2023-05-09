be9dedbb97c81ebb903f2cf3dc8c68fef3e787cf
Irina <irina.vl.kiseleva@gmail.com>
2023-05-09 12:05:20 +0300

    Remove sa_key_file

--- config.py
+++ config.py
@@ -1,49 +1,49 @@
 import logging.config
 import typing as t
 
-from pydantic import BaseSettings, Field
+from pydantic import BaseSettings
 
 from lib.logger import LOGGING
 
 
-class YDB(BaseSettings):
-    endpoint: str = 'grpcs://ydb.serverless.yandexcloud.net:2135'
-    database: str = '/ru-central1/b1g6251p1o0qlsnpmlq3/etn3m5454ilij9q4a2e9'
-    sa_key_file: str = Field(env='SA_KEY_FILE')
-
+class EnvBaseSettings(BaseSettings):
     class Config:
-        env_prefix = 'ydb_'
         env_file = '.env'
 
 
-class Dictionary(BaseSettings):
+class YDB(EnvBaseSettings):
+    endpoint: str = 'grpcs://ydb.serverless.yandexcloud.net:2135'
+    database: str
+
+    class Config:
+        env_prefix = 'ydb_'
+
+
+class Dictionary(EnvBaseSettings):
     url: str = "https://dictionary.yandex.net/api/v1/dicservice.json/lookup"
     token: str
 
     class Config:
         env_prefix = 'dictionary_'
-        env_file = '.env'
 
 
-class Translate(BaseSettings):
+class Translate(EnvBaseSettings):
     url: str = 'https://translate.api.cloud.yandex.net/translate/v2/translate'
     token: str
 
     class Config:
         env_prefix = 'translate_'
-        env_file = '.env'
 
 
-class Telegram(BaseSettings):
+class Telegram(EnvBaseSettings):
     endpoint: str = 'https://api.telegram.org'
     token: str
 
     class Config:
         env_prefix = 'telegram_'
-        env_file = '.env'
 
 
-class Settings(BaseSettings):
+class Settings(EnvBaseSettings):
     intervals: t.List[int] = [1, 2, 3, 5, 8, 13, 21, 32, 55, 89]
     ydb = YDB()
     telegram = Telegram()
